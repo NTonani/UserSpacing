3af06f0f1d013c45a995ab1f5f6f0172c57a4894
Nathan Tonani <nathantonani@gmail.com>
2018-03-15 23:11:07 -0700

    Added spacing algo

--- aws/index.js
+++ aws/index.js
@@ -4,6 +4,23 @@ const doc = require('dynamodb-doc');
 
 const dynamo = new doc.DynamoDB();
 
+const itemFormatter = (body) => {
+    var item = {};
+    item.UserId = body.uid;
+    item.URL = body.url;
+    item.CreatedMs = body.created;
+    return item;
+}
+
+const spacingAlgo = (items) => {
+    const oneDayMs = 1000 * 60 * 60 * 24; 
+    return items.filter(item => 
+        Math.log2(
+            Math.round((new Date().getTime() - item.CreatedMs)/oneDayMs)
+        ) % 1 == 0
+    );
+}
+
 exports.handler = (event, context, callback) => {
 
     const done = (err, res) => callback(null, {
@@ -24,10 +41,16 @@ exports.handler = (event, context, callback) => {
             params.ExpressionAttributeValues = {
                 ":uid":event.pathParameters.uid
             };
-            dynamo.query(params, done);
+            dynamo.query(params, function(err, res) {
+                if (err) {
+                    done(err,res);
+                } else {
+                    done(err, spacingAlgo(res.Items));
+                }
+            });
             break;
         case 'POST':
-            params.Item = event.body;
+            params.Item = itemFormatter(event.body);
             dynamo.putItem(params, done);
             break;
         default:
